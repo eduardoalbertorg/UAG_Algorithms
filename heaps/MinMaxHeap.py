d542406871323301506f0da42bde7b14b8cb3863
Eduardo Rodriguez <eduardoalbertorg@gmail.com>
2022-04-12 14:37:08 -0500

    Add big O notation

--- heaps/MinMaxHeap.py
+++ heaps/MinMaxHeap.py
@@ -3,6 +3,12 @@ class Heap(object):
         self.comparisonFunc = comparisonFunc
         self.heap = []
         self.buildHeap(array)
+    
+    def getSize(self):
+        return len(self.heap)
+    
+    def getArray(self):
+        return self.heap
         
     def getLeftChildIndex(self, index):
         return 2 * index + 1
@@ -14,10 +20,10 @@ class Heap(object):
         return (index - 1) // 2
     
     def hasLeftChild(self, index):
-        return self.getLeftChildIndex(index) < len(self.heap)
+        return self.getLeftChildIndex(index) < self.getSize()
     
-    def hasLeftChild(self, index):
-        return self.getRightChildIndex(index) < len(self.heap)
+    def hasRightChild(self, index):
+        return self.getRightChildIndex(index) < self.getSize()
     
     def hasParent(self, index):
         return self.getParentIndex(index) >= 0
@@ -35,54 +41,57 @@ class Heap(object):
     def buildHeap(self, array):
         for element in array:
             self.insert(element)
-    
-    # O(log(n)) time | O(1) space
+
+    # O(log(n)) time | O(1) space        
     def siftUp(self):
         index = len(self.heap) - 1
-        print(self.heap)
         parentIndex = self.getParentIndex(index)
         while self.hasParent(index) and self.comparisonFunc(self.heap[index], self.heap[parentIndex]):
             self.swap(index, parentIndex)
             index = parentIndex
             parentIndex = self.getParentIndex(index)
-
+                
     # O(log(n)) time | O(1) space
     def siftDown(self):
         index = 0
         while self.hasLeftChild(index):
             leftChildIndex = self.getLeftChildIndex(index)
+            rightChildIndex = self.getRightChildIndex(index)
+            indexToSwap = leftChildIndex
             
-            if self.hasRightChild(index):
-                rightChildIndex = self.getRightChildIndex(index)
-                if self.comparisonFunc(self.heap[leftChildIndex], self.heap[rightChildIndex]):
-                    indexToSwap = leftChildIndex
-                else:
-                    indexToSwap = rightChildIndex
-            
+            if self.hasRightChild(index) and self.comparisonFunc(self.heap[rightChildIndex], self.heap[leftChildIndex]):
+                indexToSwap = rightChildIndex
+
             if self.comparisonFunc(self.heap[indexToSwap], self.heap[index]):
                 self.swap(index, indexToSwap)
-                index = indexToSwap
-    
+            index = indexToSwap
+                
     # O(log(n)) time | O(1) space
     def insert(self, value):
         self.heap.append(value)
         self.siftUp()
-
-    # O(log(n)) time | O(1) space  
+        
+    # O(log(n)) time | O(1) space
     def remove(self):
-        self.swap(0, len(self.heap))
+        self.swap(0, len(self.heap) - 1)
         valueToRemove = self.heap.pop()
         self.siftDown()
         return valueToRemove
-
-    # O(1) time | O(1) space           
+    
+    # O(1) time | O(1) space
+    def peek(self):
+        return self.heap[0]
+    
+    # O(1) time | O(1) space
     def swap(self, i, j):
         self.heap[i], self.heap[j] = self.heap[j], self.heap[i]
 
 # END HEAP CLASS		
         
 def MAX_HEAP_FUNC(a, b):
+    print(f'#### Is {a} greater than {b}')
     return a > b
 
 def MIN_HEAP_FUNC(a, b):
+    print(f'#### Is {a} lesser than {b}')
     return a < b
